bbacf9ebb7fe9165832b666963de74fce48a0fe0
Oliver Fabel <28701799+ofabel@users.noreply.github.com>
2024-04-05 06:25:52 +0200

    add submodule

---
+++ applications/system/py_app/mphalport.h
@@ -0,0 +1,4 @@
+// Define so there's no dependency on extmod/virtpin.h
+#define mp_hal_pin_obj_t
+
+extern char* root_module_path;
---
+++ applications/system/py_app/py_app.c
@@ -0,0 +1,107 @@
+#include <lib/micropython/lib/micropython-build/port/micropython_embed.h>
+#include <storage/storage.h>
+#include <cli/cli.h>
+#include <furi.h>
+
+#include "mphalport.h"
+
+#define TAG "py app"
+
+char* root_module_path;
+
+static void load_python_file(const char* file_path, FuriString* code) {
+    Storage* storage = furi_record_open(RECORD_STORAGE);
+
+    File* file = storage_file_alloc(storage);
+
+    bool result = storage_file_open(file, file_path, FSAM_READ, FSOM_OPEN_EXISTING);
+
+    if(result) {
+        size_t read = 0;
+        do {
+            uint8_t buffer[64] = {'\0'};
+
+            read = storage_file_read(file, buffer, sizeof(buffer) - 1);
+
+            for(size_t i = 0; i < read; i++) {
+                furi_string_push_back(code, buffer[i]);
+            }
+        } while(read > 0);
+
+        furi_string_trim(code);
+    } else {
+        furi_string_set(code, "print('it works!')");
+    }
+
+    storage_file_free(file);
+
+    furi_record_close(RECORD_STORAGE);
+}
+
+void py_cli_execute(Cli* cli, FuriString* args, void* context) {
+    UNUSED(context);
+
+    const char* path = furi_string_get_cstr(args);
+    Storage* storage = furi_record_open(RECORD_STORAGE);
+
+    do {
+        if(furi_string_size(args) == 0) {
+            printf("Usage:\r\njs <path>\r\n");
+            break;
+        }
+
+        if(!storage_file_exists(storage, path)) {
+            printf("Can not open file %s\r\n", path);
+            break;
+        }
+
+        furi_record_close(RECORD_STORAGE);
+
+        FuriSemaphore* sem = furi_semaphore_alloc(1, 0);
+
+        printf("Running script %s, press CTRL+C to stop\r\n", path);
+
+        const size_t memory_size = memmgr_get_free_heap() * 0.5;
+        const size_t stack_size = 2 * 1024;
+        uint8_t* memory = malloc(memory_size * sizeof(uint8_t));
+
+        printf("allocated memory is %zu bytes\n", memory_size);
+        printf("stack size is %zu bytes\n", stack_size);
+
+        FuriString* file_path = args;
+        FuriString* code = furi_string_alloc();
+
+        load_python_file(path, code);
+
+        size_t index = furi_string_search_rchar(file_path, '/');
+
+        furi_check(index != FURI_STRING_FAILURE);
+
+        furi_string_left(file_path, index);
+
+        root_module_path = furi_string_get_cstr(file_path);
+
+        printf("%s", furi_string_get_cstr(code));
+
+        mp_embed_init(memory + stack_size, memory_size - stack_size, memory);
+        mp_embed_exec_str(furi_string_get_cstr(code));
+        mp_embed_deinit();
+
+        furi_string_free(code);
+        free(memory);
+
+        while(furi_semaphore_acquire(sem, 100) != FuriStatusOk) {
+            if(cli_cmd_interrupt_received(cli)) break;
+        }
+
+        furi_semaphore_free(sem);
+    } while(false);
+}
+
+void py_app_on_system_start(void) {
+#ifdef SRV_CLI
+    Cli* cli = furi_record_open(RECORD_CLI);
+    cli_add_command(cli, "py", CliCommandFlagDefault, py_cli_execute, NULL);
+    furi_record_close(RECORD_CLI);
+#endif
+}
